bc76f25d73f72fd9983fe831e4133cc05929699e
Elena <45828347+Stackframe01@users.noreply.github.com>
2021-10-22 20:38:11 +0300

    Update generateFooter.js

--- js/Modules/generateFooter.js
+++ js/Modules/generateFooter.js
@@ -3,7 +3,8 @@ const generateFooter = block => {
         <footer class="w-100 mt-2 text-center footer">
             <div class="sponsors">
                 <a href="//itmo.ru"><img class="mx-auto d-block img-fluid" src="./img/itmo_logo_horiz_white_en.png"/></a>
-                <a href="https://www.springer.com/series/8767"><img class="mx-auto d-block img-fluid" src="./img/springerlogo.jpg"/></a> 
+                <a href="https://www.springer.com/series/8767"><img class="mx-auto d-block img-fluid" src="./img/springerlogo.jpg"/></a>
+                <a href=""><img class="mx-auto d-block img-fluid" src="./img/logo_wcseit.png"/></a>
                 
             </div>
             <div>
